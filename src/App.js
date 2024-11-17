b5712a9733f1b9135d9c5681850fcbf6ffeed416
suyeon <jso8787@naver.com>
2024-11-18 02:40:45 +0900

    [#1] 프론트 백 로컬 연결

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [hello, setHello] = useState('');
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/test')
+      .then((res) => {
+        setHello(res.data);
+      }, []);
+  })
   return (
     <div className="App">
+      백엔드 데이터: {hello}
     </div>
   );
 }
