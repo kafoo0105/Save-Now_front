bd40a051fa7d9aed0b25f02837adeb3601a1cf61
suyeon <jso8787@naver.com>
2024-11-18 02:40:45 +0900

    [#1] 프론트 백 로컬 연결

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ function App() {
     axios.get('http://localhost:8080/api/test')
       .then((res) => {
         setHello(res.data);
-      }, []);
-  })
+      });
+  }, []);
+
   return (
     <div className="App">
       백엔드 데이터: {hello}
       <h1>첫 번째 커밋, 푸시 확인입니다 - 건희</h1>
       <div>커밋</div>
       <div>커밋 이정원</div>
+      <div>백엔드 데이터: {hello}</div>
     </div>
   );
 }
