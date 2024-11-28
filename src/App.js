f3972afc7c33f62e1496f8e5a06a6120a3b91b75
Kim GeonHee <kafoo0105@naver.com>
2024-11-28 17:27:38 +0900

    feat: 로그인 페이지 디자인 작업 시작 (Fixes #7)
    - 로그인 페이지의 기본 디자인 완료
    - 로그인 실패 시 작동되는 디자인 기능은 미완성
    - 카카오톡 아이콘 색상 변경은 추후 진행 예정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   const [hello, setHello] = useState('');
@@ -10,9 +12,17 @@ function App() {
       }, []);
   })
   return (
-    <div className="App">
-      백엔드 데이터: {hello}
-    </div>
+    <>
+      <div className="App">
+        백엔드 데이터: {hello}
+      </div>
+
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
